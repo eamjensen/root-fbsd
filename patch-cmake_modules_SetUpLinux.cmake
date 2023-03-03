027deb469f92d7c4d2cdf507a72ae87ad943dce9
Erik Jensen <erik@tenku.dk>
2023-03-03 20:34:34 +0100

    Patch configure step for powerpc
    Patch configure step for powerpc

--- patch-cmake_modules_SetUpLinux.cmake
+++ patch-cmake_modules_SetUpLinux.cmake
@@ -1,5 +1,5 @@
---- cmake/modules/SetUpLinux.cmake.orig	2022-11-12 10:49:59.188083000 +0100
-+++ cmake/modules/SetUpLinux.cmake	2022-11-12 10:52:12.769662000 +0100
+--- cmake/modules/SetUpLinux.cmake.orig	2023-02-13 15:21:06 UTC
++++ cmake/modules/SetUpLinux.cmake
 @@ -6,7 +6,7 @@
  
  set(ROOT_PLATFORM linux)
@@ -9,7 +9,16 @@
    if(CMAKE_CXX_COMPILER_ID STREQUAL Intel)
      set(ROOT_ARCHITECTURE linuxx8664icc)
    else()
-@@ -103,6 +103,10 @@
+@@ -23,7 +23,7 @@ elseif(CMAKE_SYSTEM_PROCESSOR MATCHES aarch64)
+   set(ROOT_ARCHITECTURE linuxarm64)
+ elseif(CMAKE_SYSTEM_PROCESSOR MATCHES arm)
+   set(ROOT_ARCHITECTURE linuxarm)
+-elseif(CMAKE_SYSTEM_PROCESSOR MATCHES ppc64)
++elseif(CMAKE_SYSTEM_PROCESSOR MATCHES ppc64 OR CMAKE_SYSTEM_PROCESSOR MATCHES powerpc64)
+   set(ROOT_ARCHITECTURE linuxppc64gcc)
+ elseif(CMAKE_SYSTEM_PROCESSOR MATCHES s390x)
+   set(ROOT_ARCHITECTURE linuxs390xgcc)
+@@ -103,6 +103,10 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL Clang)
    endif()
  
    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
